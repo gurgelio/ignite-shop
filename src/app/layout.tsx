6f97fa701887e7518bd0139783fa3b48907f43a3
Leonardo Gurgel <leo@gurgel.io>
2024-10-21 17:40:42 -0300

    add product carousel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 
 import logo from "./assets/logo.svg";
 import Image from "next/image";
