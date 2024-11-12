705b6ae6d905f6a58d6d09cdfd2593043143ab5b
Leonardo Gurgel <leo@gurgel.io>
2024-11-12 20:00:39 -0300

    add success page

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,3 +1,46 @@
-export default function Success() {
-  return <h1>Success</h1>;
+import { getSessionDetails } from "@/lib/stripe";
+import Image from "next/image";
+import Link from "next/link";
+import { notFound } from "next/navigation";
+
+interface SuccessProps {
+  searchParams: Promise<{
+    session_id?: string;
+  }>;
+}
+
+export default async function Success({ searchParams }: SuccessProps) {
+  const { session_id } = await searchParams;
+  if (session_id == null) notFound();
+  const { customer_details, line_items } = await getSessionDetails(session_id);
+  const product = line_items.data[0].price.product;
+
+  return (
+    <main className="mx-auto flex h-[656px] flex-col items-center justify-center gap-16">
+      <h1 className="text-3xl">Compra efetuada</h1>
+      <div className="flex h-32 w-full max-w-32 items-center justify-center rounded-lg bg-gradient-to-b from-[#1ea483] to-[#7465d4]">
+        <Image
+          className="object-cover"
+          src={product.images[0]}
+          alt=""
+          height={128}
+          width={128}
+          priority
+        />
+      </div>
+
+      <p className="max-w-[560px] text-center text-2xl text-zinc-300">
+        Uhuul <strong className="font-bold">{customer_details?.name}</strong>,
+        sua <strong className="font-bold">{product.name}</strong> já está a
+        caminho de sua casa.
+      </p>
+
+      <Link
+        href="/"
+        className="block text-lg font-bold text-emerald-500 hover:text-emerald-400"
+      >
+        Voltar ao catálogo
+      </Link>
+    </main>
+  );
 }
--- src/lib/stripe.ts
+++ src/lib/stripe.ts
@@ -3,6 +3,13 @@ import Stripe from "stripe";
 import { env } from "./env";
 
 export type ExpandedPriceProduct = Stripe.Product & { default_price: Stripe.Price }
+type ExpandedCheckoutSession = Stripe.Checkout.Session & {
+  line_items: Stripe.ApiList<Stripe.LineItem & {
+    price: Stripe.Price & {
+      product: Stripe.Product
+    }
+  }>
+}
 
 const stripe = new Stripe(env.STRIPE_PRIVATE_KEY, {
   apiVersion: "2024-10-28.acacia",
@@ -26,8 +33,14 @@ export async function getProducts() {
 export async function createCheckoutSession(priceId: string) {
   return stripe.checkout.sessions.create({
     mode: 'payment',
-    success_url: `${env.NEXT_URL}/success`,
+    success_url: `${env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
     cancel_url: `${env.NEXT_URL}/`,
     line_items: [{ price: priceId, quantity: 1 }]
   })
+}
+
+export async function getSessionDetails(sessionId: string) {
+  return await stripe.checkout.sessions.retrieve(sessionId, {
+    expand: ['line_items', 'line_items.data.price.product']
+  }) as ExpandedCheckoutSession
 }
